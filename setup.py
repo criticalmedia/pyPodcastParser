785288028608346b00068b7b6fe3e11188a77ae9
Steven Viola <steve.viola@criticalmedia.com>
2020-08-25 19:37:35 -0400

    Set version to match git tag

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pyPodcastParser',
 
-    version='2.0.0',
+    version='3.0.2',
 
     description='pyPodcastParser is a podcast parser.',
     long_description=long_description,
